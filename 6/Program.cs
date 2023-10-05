d5e1ad61d621be216036724decf8da4efb81ba9b
OlyaaFed <115480389+OlyaaFed@users.noreply.github.com>
2023-10-05 21:10:36 +0300

    Add files via upload

---
+++ 6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите длину последовательности");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int k = 0;
+
+int pr = 0;
+
+Console.WriteLine("Введите элементы последовательности");
+for (int i = 0; i < n; i++)
+
+
+{
+    k = Convert.ToInt32(Console.ReadLine());
+    pr = pr * k;
+
+}
+Console.WriteLine($"Произведение элементов последовательности = {pr}");
