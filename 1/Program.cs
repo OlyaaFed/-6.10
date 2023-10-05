9c9c6ee157b631e221453f6998433781045669cd
OlyaaFed <115480389+OlyaaFed@users.noreply.github.com>
2023-10-05 21:09:13 +0300

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите элементы последовательности");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int k = 0;
+
+int summa = 0;
+
+Console.WriteLine("Введите числа последовательности");
+for (int i = 0; i < n; i++)
+
+{
+   
+    k = Convert.ToInt32(Console.ReadLine());
+    summa = summa + k;
+
+}
+
+Console.WriteLine($"Сумма элементов последовательности = {summa}");
